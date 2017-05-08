e6c967bc4cf774b8fb42150206a69f80f479bbc2
John Herman Witham <john.h.witham@gmail.com>
2017-05-08 13:46:10 +0300

    Deleted Hello and Goodbye test  dummy components and routes

--- src/index.js
+++ src/index.js
@@ -9,20 +9,12 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render() {return <div>Hello!</div>}
-}
-
-class Goodbye extends React.Component {
-  render() {return <div>Goodbye!</div>}
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/Goodbye" component={Goodbye} />
+        
       </div>
   </BrowserRouter>
   </Provider>
