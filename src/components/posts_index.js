caf89646b225f942e6f9fdc73c0ec8931f6fb22e
John Herman Witham <john.h.witham@gmail.com>
2017-05-09 15:25:45 +0300

    Added mapStateToProps and connect

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -15,4 +15,8 @@ class PostsIndex extends Component {
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
